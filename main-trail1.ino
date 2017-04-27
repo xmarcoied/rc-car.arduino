3aeacc3cd227655f05f5809127ee2794c15c6890
Marko Mohsen <xmarcoied@gmail.com>
2017-04-27 15:57:07 +0200

    Init

---
+++ main-trail1.ino
@@ -0,0 +1,101 @@
+const int Motor1Pin1 = 2;
+const int Motor1Pin2 = 3;
+const int Motor2Pin1 = 4;
+const int Motor2Pin2 = 5;
+const int Motor3Pin1 = 6;
+const int Motor3Pin2 = 7;
+const int Motor4Pin1 = 8;
+const int Motor4Pin2 = 9; 
+char state = 's';
+int flag = 0;
+
+void setup() {
+  pinMode(Motor1Pin1, OUTPUT);
+  pinMode(Motor1Pin2, OUTPUT);
+  pinMode(Motor2Pin1, OUTPUT);
+  pinMode(Motor2Pin2, OUTPUT);
+  pinMode(Motor3Pin1, OUTPUT);
+  pinMode(Motor3Pin2, OUTPUT);
+  pinMode(Motor4Pin1, OUTPUT);
+  pinMode(Motor4Pin2, OUTPUT);
+  Serial.begin(9600);
+} 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
+void loop() {
+  if(Serial.available()>0){
+    state = Serial.read();
+    flag = 0;
+  } 
+  if(state == 'F'){
+    GoForward();
+  } 
+  if(state == 'B'){
+   GoBackward(); 
+  } 
+  if(state == 'R'){
+    GoRight();
+  } 
+  if(state == 'L'){
+    GoLeft();
+  } 
+  if(state == 'S'){
+    Stop();
+  } 
+}
+
+
+void GoForward() {
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, HIGH);
+  digitalWrite(Motor2Pin1, LOW);
+  digitalWrite(Motor2Pin2, HIGH);
+  digitalWrite(Motor3Pin1, HIGH);
+  digitalWrite(Motor3Pin2, LOW);
+  digitalWrite(Motor4Pin1, HIGH);
+  digitalWrite(Motor4Pin2, LOW);
+}
+
+void GoBackward() {
+  digitalWrite(Motor1Pin1, HIGH);
+  digitalWrite(Motor1Pin2, LOW);
+  digitalWrite(Motor2Pin1, HIGH);
+  digitalWrite(Motor2Pin2, LOW);
+  digitalWrite(Motor3Pin1, LOW);
+  digitalWrite(Motor3Pin2, HIGH);
+  digitalWrite(Motor4Pin1, LOW);
+  digitalWrite(Motor4Pin2, HIGH);
+}
+
+void GoLeft() {
+  digitalWrite(Motor1Pin1, HIGH);
+  digitalWrite(Motor1Pin2, LOW);
+  digitalWrite(Motor2Pin1, LOW);
+  digitalWrite(Motor2Pin2, HIGH);
+  digitalWrite(Motor3Pin1, HIGH);
+  digitalWrite(Motor3Pin2, LOW);
+  digitalWrite(Motor4Pin1, LOW);
+  digitalWrite(Motor4Pin2, HIGH);
+}
+
+void GoRight() {
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, HIGH);
+  digitalWrite(Motor2Pin1, HIGH);
+  digitalWrite(Motor2Pin2, LOW);
+  digitalWrite(Motor3Pin1, LOW);
+  digitalWrite(Motor3Pin2, HIGH);
+  digitalWrite(Motor4Pin1, HIGH);
+  digitalWrite(Motor4Pin2, LOW);
+}
+
+void Stop() {
+  digitalWrite(Motor1Pin1, LOW);
+  digitalWrite(Motor1Pin2, LOW);
+  digitalWrite(Motor2Pin1, LOW);
+  digitalWrite(Motor2Pin2, LOW);
+  digitalWrite(Motor3Pin1, LOW);
+  digitalWrite(Motor3Pin2, LOW);
+  digitalWrite(Motor4Pin1, LOW);
+  digitalWrite(Motor4Pin2, LOW);
+}
